8edb9335beae027b10d378804fe564dd37199261
varun28sharma <10dvarunsharma@gmail.com>
2025-08-08 00:25:17 +0530

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/BidVerse-A-Bidding-Website-main/' : '/',
+  base: '/FSAD-PROJECT',
   server: {
     port: 5174
   }
